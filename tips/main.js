691c425eec6b5ff8de92f6c9a627a58eea874eb5
(✿◕‿◕) <114787102+rizegm@users.noreply.github.com>
2022-10-28 23:03:56 +0800

    upload files

---
+++ tips/main.js
@@ -0,0 +1,8 @@
+const preloadWrap = document.querySelector('.preloader-wrapper')
+
+window.addEventListener('load', function () {
+    preloadWrap.classList.add('fade-out-animation');
+
+});
+Footer
+© 2022 GitHub, Inc.
