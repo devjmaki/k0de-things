481d49c6450eb9c02fbc94ab9c512b67ab7ab576
(✿⌓ ‿ ⌓) <114787102+ggmakie@users.noreply.github.com>
2022-11-11 00:57:05 +0800

    Update main.js

--- kode-exercises/s13 - js operators/main.js
+++ kode-exercises/s13 - js operators/main.js
@@ -4,6 +4,8 @@ Essay Part:
 1.What is the difference between a comparison operator and an assignment operator?
 
 2.What is a Boolean? What does it represent?
+- Boolean is a simple yet frequently used primitive data type which has two values that are deemed special words in JavaScript: True or False. 
+  Moreover, it plays a significant role in performing logic and enabling different outcomes in our code. 
 
 3.How is the += operator different than + operator?
 */
