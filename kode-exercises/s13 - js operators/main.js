3cfa9611cd1b51fd175e33b6b896006c08d9b80d
(✿⌓ ‿ ⌓) <114787102+ggmakie@users.noreply.github.com>
2022-11-11 00:24:24 +0800

    js operators

---
+++ kode-exercises/s13 - js operators/main.js
@@ -0,0 +1,18 @@
+/*
+Essay Part:
+
+1.What is the difference between a comparison operator and an assignment operator?
+
+2.What is a Boolean? What does it represent?
+
+3.How is the += operator different than + operator?
+*/
+ 
+
+//Code Part
+
+//1.Set two variables equal to two different numbers. Use a comparison operator to compare these two variables. Change one of their values by using the +=, -=, *=, or /= operator. Then, compare their values again.
+
+//2.Try out the <= and >= operators.
+
+//3.Try to use the !== operator.
