c616ad25153993f32ea8c59c8ebc811f96f6fee4
(✿⌓ ‿ ⌓) <114787102+ggmakie@users.noreply.github.com>
2022-11-12 23:49:08 +0800

    update main.js

--- kode-exercises/s15 - repetition structure/main.js
+++ kode-exercises/s15 - repetition structure/main.js
@@ -1,1 +1,59 @@
-//
+console.log("hello, world"); 
+  
+  
+ //ACTIVITY 1 
+  
+ //Prompt a user to enter a number. 
+ let number = prompt ("Give me a number"); 
+  
+ for (number; number >= 0; number--) 
+ { 
+     
+  
+     if (number < 50){ 
+         console.log(`Currently at number ${number}. Terminating the loop`); 
+         break;  
+     } 
+  
+     // if (number % 10 === 0) 
+     // { 
+     //     console.log(`The number is divisible by 10. Skipping the number`); 
+     //     continue; 
+     // } 
+  
+  
+     if (number % 5 === 0) 
+     { 
+         console.log(number); 
+     } 
+  
+ } 
+  
+  
+ //ACTIVITY 2 
+  
+ //Prompt a user to enter a number. 
+ // let number = prompt ("Give me a number"); 
+  
+ // for (number; number >= 0; number--) 
+ // { 
+     
+  
+ //     if (number < 50){ 
+ //         console.log(`Currently at number ${number}. Terminating the loop`); 
+ //         break;  
+ //     } 
+  
+     // if (number % 10 === 0) 
+     // { 
+     //     console.log(`The number is divisible by 10. Skipping the number`); 
+     //     continue; 
+     // } 
+  
+  
+ //     if (number % 5 === 0) 
+ //     { 
+ //         console.log(number); 
+ //     } 
+  
+ // }  
