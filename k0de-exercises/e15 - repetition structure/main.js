e40b70982443a6258505bad7fb67810160ce78f7
garingjem <jemkrizelle.garing@g.batstate-u.edu.ph>
2022-11-23 14:30:29 +0800

    update bootcamp files

---
+++ k0de-exercises/e15 - repetition structure/main.js
@@ -0,0 +1,54 @@
+console.log("hello, world"); 
+  
+//Prompt a user to enter a number. 
+let number = prompt ("Give me a number"); 
+  
+for (number; number >= 0; number--) 
+{ 
+     
+  
+    if (number < 50){ 
+        console.log(`Currently at number ${number}. Terminating the loop`); 
+        break;  
+    } 
+  
+    // if (number % 10 === 0) 
+    // { 
+    //     console.log(`The number is divisible by 10. Skipping the number`); 
+    //     continue; 
+    // } 
+  
+  
+    if (number % 5 === 0) 
+    { 
+        console.log(number); 
+    } 
+  
+} 
+  
+  
+//Prompt a user to enter a number. 
+// let number = prompt ("Give me a number"); 
+  
+// for (number; number >= 0; number--) 
+// { 
+     
+  
+//     if (number < 50){ 
+//         console.log(`Currently at number ${number}. Terminating the loop`); 
+//         break;  
+//     } 
+ 
+    // if (number % 10 === 0) 
+    // { 
+    //     console.log(`The number is divisible by 10. Skipping the number`); 
+    //     continue; 
+    // } 
+  
+  
+//     if (number % 5 === 0) 
+//     { 
+//         console.log(number); 
+//     } 
+  
+// }  
